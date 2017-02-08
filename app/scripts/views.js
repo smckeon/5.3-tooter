a2f2c3dacf626c0ef0d1753d5f79e04da1c37300
Dan Dietz <danddietz@gmail.com>
2017-02-08 14:30:05 -0400

    writing view selectors for testability

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -5,8 +5,7 @@ var $ = require('jquery');
 var template = require('../templates/post.hbs');
 
 function PostView(){
-  console.log($('.app').length);
-  $('.app').append('<ul class="posts">');
+  $('body').append('<ul class="posts">');
 }
 PostView.prototype.showPosts = function(posts){
   console.log(posts);
